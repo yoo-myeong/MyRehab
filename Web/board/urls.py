2b156581beed26e0dcb5f5963770e6a787a2596f
yoo-myeong <yoou201@naver.com>
2021-09-16 00:20:24 +0900

    update readme.md

--- Web/board/urls.py
+++ Web/board/urls.py
@@ -5,7 +5,6 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.board, name='board'),
-    path('', views.board, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
     path('question/create/', views.question_create, name='question_create'),
